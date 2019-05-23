275219400075af9cb72fe98a0e0a3fd51cb725a0
fkinara <42002518+fkinara@users.noreply.github.com>
2019-05-23 08:19:31 +0700

    Add files via upload

---
+++ sampah/config.php
@@ -0,0 +1,14 @@
+<?php
+
+$server = "localhost";
+$user = "root";
+$password = "";
+$dbname= "kinar";
+
+$db = mysqli_connect($server, $user, $password, $dbname);
+
+if( !$db ){
+    die("Gagal terhubung dengan database: " . mysqli_connect_error());
+}
+
+?>
